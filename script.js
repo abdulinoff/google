0d5f7e8edd0d0d18e8b33a8d14c84ac58721f743
Алан <101114785+abdulinoff@users.noreply.github.com>
2022-06-27 04:24:14 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+// function Login(){
+//     if (label.style.display == "block")
+//         document.getElementById("data__label").style.display = "none";
+//     login__ = login;
+// }
+
+// function viewDiv(){
+//     if (label.style.display == "block")
+//         document.getElementById("data__label").style.display = "none";
+//     else
+//         document.getElementById("data__label").style.display = "block";
+// }
+
+// let log = document.getElementById("info");
+
+// let button = document.getElementById("button");
+
+let button_next = document.getElementById('button_next').onclick = login;
+let data_input = document.getElementById('data_input');
+
+function login(){
+    let data_input = document.getElementById('data_input').value;
+    let latin = 'qwertyuiopasdfghjklzxcvbnm'
+    
+    // for (i of latin) {
+    //     i = i.split()
+    //     // console.log(i)
+    //     for (item of data_input) {
+    //         item = item.split()
+    //         // console.log(item)
+    //         // console.log(data_input[0])
+    //         console.log(item)
+            
+    //     }
+    
+    // }
+
+    console.log( data_input );
+    
+    // for (i of latin) {
+        
+    // }
+    // for (item of data_input) {
+       
+
+    // }
+    
+}
+// var ar = ['qwertyuiopasdfghjklzxcvbnm'];
+// var sum = 0;
+// for (var i=0, len=ar.length; i<len; i++) {
+//     if ( typeof ar[i] === 'number' ) {
+//         sum += ar[i];
+//     }
+// }
+// console.log( sum );
+
+// let button__next = document.getElementById('button__next');
+
+// let latin = ['qwertyuiopasdfghjklzxcvbnm']
+
+
+// latin.forEach(function(person){
+//     console.log(person[0])
+// })
+
+// data_input.value = ['']
+
+// data_input.forEach(function(person){
+//     console.log(person[0])
+// })
+
+
+// function login(){
+//     console.log(data_input.value)
+// }
